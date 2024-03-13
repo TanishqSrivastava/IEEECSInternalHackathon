5e6d49d28cdc1127d6fc69d4333a81c5b24eb061
Tanishq Srivastava <yashu_comp@hotmail.com>
2024-03-13 19:15:18 +0530

    Finished functionality of forums and basic frontend work is done

---
+++ client/src/components/common/jumbotron.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Jumotron = () => {
+  return (
+    <div className="jumbotron">
+      <h1 className="display-3">Header</h1>
+      
+    </div>
+  );
+};
+
+export default Jumotron;
